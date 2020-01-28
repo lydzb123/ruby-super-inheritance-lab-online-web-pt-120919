21a8565d4f9bd2211a621e7705f9c03526921517
Lydia B <lydiabaik@gmail.com>
2020-01-28 16:43:52 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -6,9 +6,7 @@ class ChattyStudent < Student
     super
     puts "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay
 , but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell
-you who died...\n" to stdout, but output "Hey there! I'm so excited to learn stuff.\nHey there! I'm so excite
-d to learn stuff. How are you doi..., it was so crazy! What, you don't want any spoilers? Okay well let me ju
-st tell\nyou who died...\n"
+you who died...\n"
   end
 
 end
