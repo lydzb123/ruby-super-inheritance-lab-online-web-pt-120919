8471f2894c8338b4752571b7af8dc8cfff36f6e3
Lydia B <lydiabaik@gmail.com>
2020-01-28 16:42:22 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -4,6 +4,9 @@ class ChattyStudent < Student
 
   def hello
     super
+    puts "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay
+, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell
+you who died...\n" to stdout, but output "Hey there! I'm so excited to learn stuff.
   end
 
 end
