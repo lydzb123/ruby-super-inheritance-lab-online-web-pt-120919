60ff8dc19362141aee076c8829f2ce10e8d9124b
Lydia B <lydiabaik@gmail.com>
2020-01-28 16:35:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,16 +1,16 @@
 class Student
   attr_accesor :name
-  
+
   def initialize(name)
     @name = name
   end
 
   def hello
-    "Hey there! I'm so excited to learn stuff."
+    return "Hey there! I'm so excited to learn stuff."
   end
 
   def raise_hand
-    "Pick me!"
+    return "Pick me!"
   end
 
 end
